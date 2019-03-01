9ab169f02c20c01f22d6d504cabe067588bb008e
Forrest <forrest.tennant@gmail.com>
2019-03-01 12:28:58 -0600

    Main menu ui created. Functionality not yet finished.

--- app/src/main/java/edu/uark/uarkregisterapp/MainActivity.java
+++ app/src/main/java/edu/uark/uarkregisterapp/MainActivity.java
@@ -210,10 +210,10 @@ public class MainActivity extends AppCompatActivity {
         callApiButton.setVisibility(View.INVISIBLE);
         signOutButton.setVisibility(View.VISIBLE);
         //findViewById(R.id.welcome).setVisibility(View.VISIBLE);
-        ((TextView) findViewById(R.id.welcome)).setText("Welcome, " +
-                authResult.getAccount().getUsername());
-        //setContentView(R.layout.activity_landing);
-        //this.startActivity(new Intent(getApplicationContext(), LandingActivity.class));
+//        ((TextView) findViewById(R.id.welcome)).setText("Welcome, " +
+//                authResult.getAccount().getUsername());
+        setContentView(R.layout.activity_landing);
+        this.startActivity(new Intent(getApplicationContext(), LandingActivity.class));
         findViewById(R.id.graphData).setVisibility(View.VISIBLE);
     }
 
